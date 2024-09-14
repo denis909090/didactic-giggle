03a9624c9e7580367dfa4fea2f886a652fd80ebe
Denis <135863448+denis909090@users.noreply.github.com>
2024-09-14 23:21:13 +0300

    iuh

---
+++ js/my.js
@@ -0,0 +1,29 @@
+const targetBtn = document.querySelector(".js-target-btn");
+const addListenerBtn = document.querySelector(".js-add-listener");
+const removeListenerBtn = document.querySelector(".js-remove-listener");
+
+// function onTargetBtnClick() {
+//   targetBtn.addEventListener("click", () => {
+//     console.log("простой клик без доп-вешания");
+//   });
+// }
+// onTargetBtnClick()
+
+// addListenerBtn.addEventListener("click", () => {
+//   console.log("Вешаю слушателя события на целевую кнопку");
+
+//   targetBtn.addEventListener("click", () => {
+//     console.log("Клик по целевой кнопке");
+//   });
+// });
+
+// removeListenerBtn.addEventListener("click", () => {
+//   console.log("удаляю прослушку");
+//     targetBtn.removeEventListener("click", () => {
+//         console.log("удалил прослушку");
+//   });
+// });
+
+//  
+
+
